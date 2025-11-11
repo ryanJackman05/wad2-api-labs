8be906a7d4e4b5fb5d538b63adb2c361079fbf81
ryanJackman05 <20108256@mail.wit.ie>
2025-11-11 12:21:28 +0000

    added routing for posting a new task

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { tasksData } from './tasksData';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
@@ -14,6 +15,21 @@ router.get('/:id', (req, res) => {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
     return res.status(200).json(task);
+});
+//Add a task
+router.post('/', (req, res) => { // http POST handler for adding a new task, and then returning it with a 201 status code
+    const { title, description, deadline, priority, done } = req.body; // look for params in request body
+    const newTask = { // receive object from request body,
+        id: uuidv4(), // add unique id
+        title,
+        description,
+        deadline,
+        priority,
+        done
+    };
+    tasksData.tasks.push(newTask); // instance, not stored anywhere?
+    res.status(201).json(newTask); // can these methods be swapped?
+    tasksData.total_results++;
 });
 
 export default router;
