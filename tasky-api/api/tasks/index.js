c2fdfca9a5cddf0bab938328515f9f63780e45dc
ryanJackman05 <20108256@mail.wit.ie>
2025-11-11 12:41:10 +0000

    Added routing for Updating and Deleting a Task

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { tasksData } from './tasksData';
+import { tasksData } from './tasksData'; // import the tasksData object from tasksData.js
 import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
@@ -27,9 +27,31 @@ router.post('/', (req, res) => { // http POST handler for adding a new task, and
         priority,
         done
     };
-    tasksData.tasks.push(newTask); // instance, not stored anywhere?
-    res.status(201).json(newTask); // can these methods be swapped?
+    tasksData.tasks.push(newTask); // NOT instance. the tasksData object was imported from tasksData.js, but now exists as a variable accessible here.
+    res.status(201).json(newTask); // can these methods be swapped? YES
     tasksData.total_results++;
+});
+//Update an existing task
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const taskIndex = tasksData.tasks.findIndex(task => task.id === id); 
+    if (taskIndex === -1) {
+        return res.status(404).json({ status: 404, message: 'Task not found' });
+    }
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id }; // overwrites the task's body with request body. (if request has a field that task does, that field is the new value for task)
+    tasksData.tasks[taskIndex] = updatedTask; // overwrite the task in tasksData with updatedTask
+    res.json(updatedTask);
+});
+//Delete a task
+router.delete('/:id', (req, res) => {
+    const { id } = req.params; // like update, looks specifically for id param
+    const taskIndex = tasksData.tasks.findIndex(task => task.id === id);
+    
+    if (taskIndex === -1) return res.status(404).json({status:404,message:'Task not found'});
+    
+    tasksData.tasks.splice(taskIndex, 1);
+    res.status(204).send(); // 204 - no content
+    tasksData.total_results--;
 });
 
 export default router;
